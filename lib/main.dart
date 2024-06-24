eacd1536dbd734328695c2b53e0febc871637cb4
gichunga <gichungasteve6@gmail.com>
2024-06-24 12:06:17 +0300

    implement hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("My First App"),
-      centerTitle: true,
-      backgroundColor: Colors.teal[800],
-    ),
-    body: Center(
-      child: Text(
-        'This is the body content',
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          fontSize: 20.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower'
+void main() => runApp(const MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My First App"),
+        centerTitle: true,
+        backgroundColor: Colors.teal[800],
+      ),
+      body: Center(
+        child: Text(
+          'This is the body content',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              fontSize: 20.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower'
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: Text("click"),
-      backgroundColor: Colors.teal[800],
-    ),
-  ),
-));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        backgroundColor: Colors.teal[800],
+        child: const Text(
+          "click",
+          style: TextStyle(
+            color: Colors.white
+          ),
+        ),
+      ),
+    );
+  }
+}
